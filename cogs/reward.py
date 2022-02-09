7c47cd62214f7a3cf0a4f1a1faa903918f990832
MrRazamataz <mrrazamataz@mrrazamataz.ga>
2022-02-09 23:30:58 +0000

    add mee6 level support

--- cogs/reward.py
+++ cogs/reward.py
@@ -30,38 +30,73 @@ class reward(commands.Cog):
         rconpassword = config["rconpassword"]
         global prefix
         prefix = config["prefix"]
+        global use_mee6_levels
+        use_mee6_levels = config["use_mee6_levels"]
         global accountdata
         with open("accountdata.json") as f:
             accountdata = json.load(f)
     @commands.Cog.listener()
     async def on_message(self, message):
-        global levels, accountdata
+        global levels, accountdata, use_mee6_levels, prefix
         if message.guild is None:
             return
-        if not message.author.bot:
-            id = str(message.author.id)
-            levels[id] = levels.get(id, 0.99) + 0.01
-            global recent_change, prefix
-            recent_change = True
-            if levels[id].is_integer():
-                level = round(levels[id])
-                rank = "rank"
-                lp_group = f"{level}-{rank}"
-                await message.channel.send(f"{message.author.mention}, well done! You're now level: `{level}`.")
-                if str(message.author.id) in accountdata:
-                    if str(level) in rewards:
-                        command = rewards[f"{str(level)}"]["command"]
-                        try:
-                            command = command.replace("%player%", f"{accountdata[id]}")
-                        except:
-                            pass
-                        #await message.channel.send(f"The command: \n`{command}`")
-                        with MCRcon(f"{rconip}", f"{rconpassword}", port=rconport) as mcr:
-                            resp = mcr.command(f"{command}")
-                else:
-                    await message.channel.send(f"Please link your Minecraft account to your Discord with `{prefix}link <username>`!")
-                #with MCRcon(f"{rconip}", f"{rconpassword}", port=rconport) as mcr:
-                    #resp = mcr.command(f"lp user {accountdata[id]} parent add {rank}")
+        if use_mee6_levels == "false":
+            if not message.author.bot:
+                id = str(message.author.id)
+                levels[id] = levels.get(id, 0.99) + 0.01
+                global recent_change, prefix
+                recent_change = True
+                if levels[id].is_integer():
+                    level = round(levels[id])
+                    rank = "rank"
+                    lp_group = f"{level}-{rank}"
+                    await message.channel.send(f"{message.author.mention}, well done! You're now level: `{level}`.")
+                    if str(message.author.id) in accountdata:
+                        if str(level) in rewards:
+                            command = rewards[f"{str(level)}"]["command"]
+                            try:
+                                command = command.replace("%player%", f"{accountdata[id]}")
+                            except:
+                                pass
+                            # await message.channel.send(f"The command: \n`{command}`")
+                            with MCRcon(f"{rconip}", f"{rconpassword}", port=rconport) as mcr:
+                                resp = mcr.command(f"{command}")
+                    else:
+                        await message.channel.send(f"Please link your Minecraft account to your Discord with `{prefix}link <username>`!")
+                    #with MCRcon(f"{rconip}", f"{rconpassword}", port=rconport) as mcr:
+                        #resp = mcr.command(f"lp user {accountdata[id]} parent add {rank}")
+
+        elif use_mee6_levels == "true":
+            if message.author.id == 159985870458322944:
+                if "you just advanced to level" in message.content:
+                    print("Attempting to hook MEE6 level update...")
+                    if message.mentions:
+                        for users in message.mentions:
+                            find_level = message.content
+                            find_level = find_level.replace(f"{users.id}", "USER")
+                            for words in find_level:
+                                for letters in words:
+                                    try:
+                                        level_num = int(letters)
+                                        print(f"Level: {level_num}")
+                                    except Exception as e:
+                                        # yes yes bad practice
+                                        pass
+                            if str(users.id) in accountdata:
+                                if str(level_num) in rewards:
+                                    command = rewards[f"{str(level_num)}"]["command"]
+                                    try:
+                                        command = command.replace("%player%", f"{accountdata[str(users.id)]}")
+                                    except:
+                                        pass
+                                    # await message.channel.send(f"The command: \n`{command}`")
+                                    with MCRcon(f"{rconip}", f"{rconpassword}", port=rconport) as mcr:
+                                        resp = mcr.command(f"{command}")
+                            else:
+                                await message.channel.send(
+                                    f"Please link your Minecraft account to your Discord with `{prefix}link <username>`!")
+                    else:
+                        print("Failed to hook! No pings detected. It's currently not possible to get the user without a @user ping.")
 
     @commands.command(name="addreward")
     @commands.has_permissions(administrator=True)
